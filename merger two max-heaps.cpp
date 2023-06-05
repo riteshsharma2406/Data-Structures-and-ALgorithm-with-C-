a7a89068f7c65d6cecad3362cb45cd11b873d9d9
Ritesh Sharma <101566115+riteshsharma2406@users.noreply.github.com>
2023-06-05 17:28:54 +0530

    Create merger two max-heaps.cpp

---
+++ merger two max-heaps.cpp
@@ -0,0 +1,25 @@
+class Solution{
+    public:
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        // your code here
+        priority_queue<int>pq;
+        vector<int>ans;
+        
+        for(int i=0; i<n; i++)
+        {
+            pq.push(a[i]);
+        }
+        
+        for(int i=0; i<m; i++)
+        {
+            pq.push(b[i]);
+        }
+        
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
