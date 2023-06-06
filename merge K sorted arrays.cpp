9577e1d6f100c8f46c96144f00754a06706fae92
Ritesh Sharma <101566115+riteshsharma2406@users.noreply.github.com>
2023-06-06 18:32:06 +0530

    Update merge K sorted arrays.cpp

--- merge K sorted arrays.cpp
+++ merge K sorted arrays.cpp
@@ -1,28 +1,75 @@
-class Solution
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class info{
+  public:
+  int value;
+  int row;
+  int col;
+
+  info(int val, int r, int c)
 {
-    public:
-    //Function to merge k sorted arrays.
-    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
-    {
-        //code here
-        priority_queue<int,vector<int>,greater<int>>pq;
-        
-        for(int i=0; i<K; i++)
-        {
-            for(int j=0; j<K; j++)
-            {
-                pq.push(arr[i][j]);
-            }
-        }
-        
-        vector<int>ans;
-        
-        while(!pq.empty())
-        {
-            ans.push_back(pq.top());
-            pq.pop();
-        }
-        
-        return ans;
-    }
+  value = val;
+  row = r;
+  col = c;
+}
+
 };
+
+class compare{
+  public:
+  bool operator()(info* a, info* b)
+{
+  return a->value > b->value;
+}
+};
+
+vector<int> mergeKSortedArray(int arr[][4], int k, int n)
+{
+  priority_queue<info* , vector<info*>, compare> minHeap;
+
+  //insert every element first element
+  for(int i=0; i<k; i++)
+    {
+      info *temp = new info(arr[i][0], i,0);
+      minHeap.push(temp);
+    }
+  vector<int>ans;
+
+  while(!minHeap.empty())
+  {
+    info *temp = minHeap.top();
+    int topElement  = temp -> value;
+    int topRow = temp ->row;
+    int topCol = temp -> col;
+    minHeap.pop();
+
+    ans.push_back(topElement);
+
+    if(topCol +1 < n)
+    {
+      info *newInfo = new info(arr[topRow][topCol+1], topRow, topCol+1);
+      minHeap.push(newInfo);
+    }
+  }
+
+  return ans;
+}
+
+
+ 
+int main() {
+  int arr[][4] = {{2,4,6,8},{1,3,5,7},{8,9,10,11}};
+  int k = 3;
+  int n = 4;
+  vector<int>ans = mergeKSortedArray(arr,k,n);
+
+  for(auto i: ans)
+    {
+      cout<<i<<" ";
+    }
+
+  return 0;
+}
