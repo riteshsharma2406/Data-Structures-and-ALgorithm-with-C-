3b1d38aebdad64f216a0a89be70d243134af507c
Ritesh Sharma <101566115+riteshsharma2406@users.noreply.github.com>
2023-06-06 18:31:17 +0530

    Create Merge k Sorted Lists.cpp

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class compare
+{
+    public:
+    bool operator()(ListNode* a, ListNode* b)
+    {
+        return (a->val > b->val);
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare>minHeap; 
+
+        int k = lists.size();
+        if(k==0)
+        {
+            return NULL;
+        }
+
+        //insert first element of every linked list
+        for(int i=0; i<k; i++)
+        {
+            if(lists[i]!=NULL)
+            {
+                minHeap.push(lists[i]);
+            }
+        }
+
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        while(!minHeap.empty())
+        {
+            ListNode* temp = minHeap.top();
+            minHeap.pop();
+
+            //temp may or may not be first element of answer linked list
+            if(head==NULL)
+            {
+                //temp is first element
+                head = temp;
+                tail = temp;
+                if(tail->next!=NULL)
+                {
+                    minHeap.push(tail->next);
+                }
+            }
+            else
+            {
+                //temp is not the first element of Linked List
+                tail->next = temp;
+                tail = temp;
+                if(tail->next!=NULL)
+                {
+                    minHeap.push(tail->next);
+                }
+
+            }
+        }
+        return head;
+
+    }
+};
